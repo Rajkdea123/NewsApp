897f6c87c0ecc650ea2cb97825ae797aa5a0825e
Rajkdea123 <ravindrayadav78795@gmail.com>
2024-03-11 14:05:16 +0530

    first commint

---
+++ lib/Controller/BottomNavigationController.dart
@@ -0,0 +1,16 @@
+import 'package:get/get.dart';
+
+import '../Pages/ArticalPage/ArticalPage.dart';
+import '../Pages/HomePage/HomePage.dart';
+import '../Pages/Profile/ProfilePage.dart';
+
+
+class BottomNavController extends GetxController {
+  RxInt index = 0.obs;
+
+  var pages = [
+    const HomePage(),
+    const ArticalPage(),
+    const ProfilePage(),
+  ];
+}
